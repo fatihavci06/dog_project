c82650e6b43efe436821cac721e5b815689024b7
Fatih <64631921+fatihavci06@users.noreply.github.com>
2026-01-06 16:29:12 +0300

    email asenkron

--- app/Mail/VerifyEmailMail.php
+++ app/Mail/VerifyEmailMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\User;
 
-class VerifyEmailMail extends Mailable
+class VerifyEmailMail extends Mailable implements \Illuminate\Contracts\Queue\ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -19,6 +19,7 @@ class VerifyEmailMail extends Mailable
         $this->user = $user;
 
         $this->url = url("/api/verify-email/{$user->id}/" . sha1($user->email));
+        $this->afterCommit = true;
     }
 
     public function build()
